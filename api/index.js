a2fd1a67f939b3b561196cf173da7de29e560fac
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-05 15:57:57 -0400

    Base Api Vercel

---
+++ api/index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(4000, () => {
+  console.log('Server started on http://localhost:4000');
+});
+
+module.exports = app;
