2796dda0b36ad56c8cbf52c0a825e8ad39efa9dc
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-05 17:35:32 -0400

    Api Implemented

--- api/index.js
+++ api/index.js
@@ -1,14 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "../config/db.js";
+import productRoutes from "./routes/products.route.js";
 
-import express from 'express';
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Express on Vercel');
-});
+const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-  console.log('Server started on http://localhost:4000');
-});
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
 
 export default app;
