17720e4dac7906b0f4c642a3df8bc88e28e93d99
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-05 17:44:31 -0400

    cors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors'
 import { connectDB } from "../config/db.js";
 import productRoutes from "./routes/products.route.js";
 
@@ -8,6 +9,8 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors);
 
 app.use(express.json());
 
