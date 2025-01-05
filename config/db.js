cd414be00d2d0303460f42c7e77e76ec6e7ebaa8
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-05 17:40:44 -0400

    methods implemented

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB conexion ${conn.connection.host}`);
+    console.log(`MongoDBt conexion ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
